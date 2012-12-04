8919705ec60e8b4b2e81c7734e9313b480f405bc
Ivan Osmak <ivan.osmak@telerik.com>
2012-12-04 14:56:46 +0200

    Added ResourceEnumerator2 class.
    Added ResourceEnumerator2 class.

---
+++ Patches/ImplicitLocalization/ResourceEnumerator2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitefinityWebApp.Patches.ImplicitLocalization
+{
+    public class ResourceEnumerator2 : IDictionaryEnumerator, IEnumerator
+    {
+        public DictionaryEntry Entry
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object Key
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object Value
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
