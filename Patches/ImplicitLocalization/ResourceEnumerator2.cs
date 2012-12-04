f065b32ee362ff1f3bfe8bc573d5c43302ec5860
Ivan Osmak <ivan.osmak@telerik.com>
2012-12-04 15:55:02 +0200

    Implemented basic scenario for MoveNext
    One resource file with two entries can be read.

--- Patches/ImplicitLocalization/ResourceEnumerator2.cs
+++ Patches/ImplicitLocalization/ResourceEnumerator2.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+using System.Xml;
 
 namespace SitefinityWebApp.Patches.ImplicitLocalization
 {
     public class ResourceEnumerator2 : IDictionaryEnumerator
     {
+        #region Construction
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ResourceEnumerator2"/> type for the
+        /// provided resourceFilePaths.
+        /// </summary>
+        /// <param name="resourceFilePaths"></param>
+        public ResourceEnumerator2(string[] resourceFilePaths)
+        {
+            if (resourceFilePaths == null)
+                throw new ArgumentNullException("resourceFilePaths");
+
+            if (resourceFilePaths.Length == 0)
+                throw new ArgumentException("At least one resource file path must be passed.");
+
+            this.resourceFilePaths = resourceFilePaths;
+        }
+
+        #endregion
+
         #region IDictionaryEnumerator members
 
         /// <summary>
@@ -66,13 +87,55 @@ namespace SitefinityWebApp.Patches.ImplicitLocalization
             }
         }
 
+        /// <summary>
+        /// Advances the enumerator to the next element of the collection.
+        /// </summary>
+        /// <exception cref="T:System.InvalidOperationException">The collection was modified
+        /// after the enumerator was created. </exception>
+        /// <returns>
+        /// true if the enumerator was successfully advanced to the next element;
+        /// false if the enumerator has passed the end of the collection.
+        /// </returns>
         public bool MoveNext()
         {
-            throw new NotImplementedException();
+            if (this.xmlReader == null)
+                this.xmlReader = XmlReader.Create(this.resourceFilePaths[0]);
+
+            this.key = null;
+            this.value = null;
+
+            while (this.xmlReader.Read())
+            {
+                if (this.xmlReader.NodeType == XmlNodeType.Element && this.xmlReader.Name == "data")
+                {
+                    this.xmlReader.MoveToAttribute("name");
+                    this.key = this.xmlReader.Value;
+
+                    while (this.xmlReader.Read())
+                    {
+                        if (this.xmlReader.NodeType == XmlNodeType.Element && this.xmlReader.Name == "value")
+                        {
+                            this.xmlReader.Read();
+                            this.value = this.xmlReader.Value;
+                        }
+                        if (this.xmlReader.NodeType == XmlNodeType.EndElement && this.xmlReader.Name == "data")
+                            break;
+                    }
+                    break;
+                }
+            }
+
+            if (this.xmlReader.EOF)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public void Reset()
         {
+            // close the reader; go back to the first file
             throw new NotImplementedException();
         }
 
@@ -82,6 +145,8 @@ namespace SitefinityWebApp.Patches.ImplicitLocalization
 
         private object key;
         private object value;
+        private XmlReader xmlReader;
+        private string[] resourceFilePaths;
 
         #endregion 
     }
