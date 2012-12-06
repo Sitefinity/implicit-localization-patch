46532fa256ef2994ee04e021d842005e162af1df
Ivan Osmak <ivan.osmak@telerik.com>
2012-12-06 02:01:11 +0200

    Gave up on merging

--- Patches/ImplicitLocalization/IResourceFileResolver.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace SitefinityWebApp.Patches.ImplicitLocalization
-{
-    public interface IResourceFileResolver
-    {
-        string[] ResolveResourceFilePaths(string virtualPath);
-    }
-}
